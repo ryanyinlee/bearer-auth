30b2f49ead76da25e62e9a5d700a9cd250c50020
Ryan Lee <ryanyinlee@protonmail.com>
2022-01-19 07:56:07 -0800

    14 out of 22

--- src/auth/routes.js
+++ src/auth/routes.js
@@ -1,35 +1,38 @@
 'use strict';
 
 const express = require('express');
-const authRouter = express.Router();
+const app = express(); // added 
+app.use(express.json()); // added
+const authRouter = express.Router(); 
 
-const { users } = require('./models/index.js');
+const { userModel } = require('./models/index.js'); // from users to userModel also changed require to just the folder: Removed 
 const basicAuth = require('./middleware/basic.js')
 const bearerAuth = require('./middleware/bearer.js')
 
 authRouter.post('/signup', async (req, res, next) => {
   try {
-    let userRecord = await users.create(req.body);
+    let userRecord = await userModel.create(req.body);  // from users to Users 
     const output = {
-      user: userRecord,
+      user: userRecord, // user from users
       token: userRecord.token
     };
-    res.status(200).json(output);
+    res.status(201).json(output); // changed 200 to 201
   } catch (e) {
+    console.log("In routes.js, /signup route error: " + e);
     next(e.message);
   }
 });
 
-authRouter.post('/signin', basicAuth, (req, res, next) => {
+authRouter.post('/signin', basicAuth, async (req, res, next) => { // added async before req,res
   const user = {
-    user: request.user,
-    token: request.user.token
+    user: req.user,
+    token: req.user.token
   };
-  res.status(200).json(user);
+  res.status(200).json(user); // 
 });
 
 authRouter.get('/users', bearerAuth, async (req, res, next) => {
-  const users = await Users.findAll({});
+  const users = await userModel.findAll({});
   const list = users.map(user => user.username);
   res.status(200).json(list);
 });
